b822b13b83787421a7d92f0c3c8fdfbbdc7d7af4
Stargazer123 <fredrik@stensatter.se>
2022-01-31 17:36:44 +0100

    rem fix

--- bin/startup.sh
+++ bin/startup.sh
@@ -7,8 +7,8 @@ fi
 echo $CORNELIA_HOME
 
 LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -c $CORNELIA_HOME/conf/corny.conf & 2> $CORNELIA_HOME/log/error.log
-#LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -ssl -c $CORNELIA_HOME/conf/corny.conf & 2> $CORNELIA_HOME/log/error.log
-#LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -tls -c $CORNELIA_HOME/conf/corny.conf & 2> $CORNELIA_HOME/log/error.log
+LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -ssl -c $CORNELIA_HOME/conf/corny.conf & 2> $CORNELIA_HOME/log/error.log
+LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -tls -c $CORNELIA_HOME/conf/corny.conf & 2> $CORNELIA_HOME/log/error.log
 
 
 
