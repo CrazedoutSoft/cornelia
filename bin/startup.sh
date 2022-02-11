b7640347139d0ebb45b74dbfd07316f31f1e2ea9
Stargazer123 <fredrik@stensatter.se>
2022-02-11 15:34:20 +0100

    com

--- bin/startup.sh
+++ bin/startup.sh
@@ -4,11 +4,11 @@ if [[ -z "${CORNELIA_HOME}" ]]; then
   export CORNELIA_HOME=$(pwd)
 fi
 
-echo CORNELIA_HOME=$CORNELIA_HOME
+export LD_LIBRARY_PATH=$CORNELIA_HOME/openssl
 
-LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
-LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -ssl -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
-LD_LIBRARY_PATH=$CORNELIA_HOME/openssl $CORNELIA_HOME/bin/cornelia_d -tls -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
+$CORNELIA_HOME/bin/cornelia_d -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
+$CORNELIA_HOME/bin/cornelia_d -ssl -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
+$CORNELIA_HOME/bin/cornelia_d -tls -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
 
 
 
