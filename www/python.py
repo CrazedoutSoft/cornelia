15ae15448579c9f7bb3977488cd6f28406b842cb
Stargazer123 <fredrik@stensatter.se>
2022-05-30 22:06:00 +0200

    bootstrap

--- www/python.py
+++ www/python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 print("Content-Type: text/html\r\n\r\n");
-print("Hello World")
+print("<h2>Hello Python World</h2>")
 print("\n");
 print("\n");
 
--- www/test.php
+++ www/test.php
@@ -2,6 +2,6 @@
 
 <h1>
 <?php
- echo $_SERVER["SCRIPT_FILENAME"];
+ echo "PHP Up and runnin'"
 ?>
 </h1>
