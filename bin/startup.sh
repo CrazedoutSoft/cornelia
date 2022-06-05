b875569a17a9dd3f0fe6245dae73eb586ef43de3
Stargazer123 <fredrik@stensatter.se>
2022-06-05 17:02:43 +0200

    only http default

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [[ -z "${CORNELIA_HOME}" ]]; then
   export CORNELIA_HOME=$(pwd)
 fi
@@ -9,8 +10,6 @@ export LD_LIBRARY_PATH=$CORNELIA_HOME/openssl
 if [[ $1 == "" ]];
 then
 $CORNELIA_HOME/bin/cornelia_d -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
-$CORNELIA_HOME/bin/cornelia_d -ssl -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
-$CORNELIA_HOME/bin/cornelia_d -tls -c $CORNELIA_HOME/conf/corny.conf 2> $CORNELIA_HOME/log/error.log &
 fi
 
 if [[ $1 == "http" ]];
