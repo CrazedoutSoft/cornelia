e47d214a90884f913eb28a9d19d9583cb25ca62e
Stargazer123 <fredrik@stensatter.se>
2022-06-11 13:47:03 +0200

    jgonline

---
+++ www/jgonline/jgaz.c
@@ -0,0 +1,52 @@
+#include "../../include/misc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void exec(const char* code){
+
+	FILE *fd;
+	char* file = (char*)malloc(1024);
+	char* buffer = (char*)malloc(1024);
+
+	fd = fopen("/tmp/jgaz.jg","w");
+	fputs(code, fd);
+	fclose(fd);
+
+	fd = popen("jgazm -f  /tmp/jgaz.jg 2>&1","r");
+	printf("<div style='color: white'><pre><code>\n");
+	
+	while((fgets(buffer,1024,fd))!=NULL){
+	  printf("%s", buffer);
+	}
+	fclose(fd);
+	printf("</code><pre></div>\n");
+	free(buffer);
+
+}
+
+int main(int args, char* argv[]){
+
+	char* qs = getenv("CONTENT_LENGTH");
+	char* buffer;
+	char* decoded;
+	int len;
+
+	printf("Content-Type: text/html\n\n");
+
+	if(qs!=NULL){
+	  len = atoi(qs);
+	  buffer = (char*)malloc(len+1);
+	  decoded = (char*)malloc(len+1);
+	  memset(buffer,0,len);
+	  memset(decoded,0,len);
+	  fread(buffer,len,1,stdin);
+	  decode(&buffer[5],decoded);
+	  exec(decoded);
+	  free(buffer);
+	  free(decoded);
+	}
+
+
+}
