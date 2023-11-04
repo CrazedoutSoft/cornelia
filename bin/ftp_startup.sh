13d92bc6634b0934cb6f3c66bd3bcce208d716d5
Fredrik <fredrik@stensatter.se>
2023-11-04 15:39:31 +0100

    forgot export of libpath, fixed

--- bin/ftp_startup.sh
+++ bin/ftp_startup.sh
@@ -12,6 +12,8 @@ if [[ -z "${CORNELIA_HOME}" ]]; then
   echo env 'CORNELIA_HOME' missing. Setting CORNELIA_HOME to $(pwd)
 fi
 
+export LD_LIBRARY_PATH=$CORNELIA_HOME/openssl
+
 bin/ftp_cornelia $1 $2 $3 $4 &
 
 
